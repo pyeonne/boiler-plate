25fba3898de99774605b693d367e57c9623f953c
pyeonne <pyeonne@gmail.com>
2021-10-05 23:30:33 +0900

    chore: mongo DB Model & Schema

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50,
+    },
+    email: {
+        type: String,
+        trim: true, // 공백을 없애주는 역할
+        unique: 1, // 똑같은 이메일을 사용 못하게 하는 역할
+    },
+    password: {
+        type: String,
+        minlength: 5,
+    },
+    lastname: {
+        type: String,
+        maxlength: 50,
+    },
+    // 관리자와 일반 유저를 구분하기 위함
+    role: {
+        type: Number,
+        default: 0,
+    },
+    image: String,
+    token: {
+        type: String,
+    },
+    tokenExp: {
+        type: Number,
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {};
