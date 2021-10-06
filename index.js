d9c930118505ff7e2045fa01348aceb02ed6d637
pyeonne <pyeonne@gmail.com>
2021-10-06 13:41:06 +0900

    chore: 비밀 설정 정보 관리

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const config = require('./config/key');
 const { User } = require('./models/User');
 
 // application/x-www-form-urlencoded
@@ -12,16 +13,13 @@ app.use(express.json());
 // mongo DB 연결
 const mongoose = require('mongoose');
 mongoose
-    .connect(
-        'mongodb+srv://pyeonne:djfuqek114@boilerplate.6uycn.mongodb.net/boilerplate?retryWrites=true&w=majority',
-        {
-            // mongoose 6.0 이상에서는 기본값이므로 더이상 지원하지 않는다.
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true,
-            // useCreateIndex: true,
-            // useFindAndModify: false,
-        },
-    )
+    .connect(config.mongoURI, {
+        // mongoose 6.0 이상에서는 기본값이므로 더이상 지원하지 않는다.
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+        // useCreateIndex: true,
+        // useFindAndModify: false,
+    })
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
