3723dfecd2c52f80570931373d3a5679e930234b
pyeonne <pyeonne@gmail.com>
2021-10-08 22:39:48 +0900

    feat: 로그인 라우터 구현

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const { User } = require('./models/User');
 
@@ -9,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // application/json
 app.use(express.json());
+app.use(cookieParser());
 
 // mongo DB 연결
 const mongoose = require('mongoose');
@@ -27,7 +29,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!~~안녕하세요 ~ ㅇ.ㅇ');
 });
 
-// 회원가입을 위한 라우터
+// 회원가입 라우터
 app.post('/register', (req, res) => {
     // 회원가입할 때 필요한 정보들을 client에서 가져오면
     // 그것들을 데이터 베이스에 넣어준다.
@@ -37,6 +39,32 @@ app.post('/register', (req, res) => {
         if (err) return res.json({ success: false, err });
         return res.status(200).json({
             success: true,
+        });
+    });
+});
+
+// 로그인 라우터
+app.post('/api/users/login', (req, res) => {
+    // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: '제공된 이메일에 해당하는 유저가 없습니다.',
+            });
+        }
+
+        // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인한다.
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) return res.json({ loginSuccess: false, message: '비밀번호가 틀렸습니다.' });
+
+            // 비밀번호까지 맞다면 토큰을 생성한다.
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
+                res.cookie('x_auth', user.token).status(200).json({ loginSuccess: true, userId: user._id });
+            });
         });
     });
 });
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -48,8 +49,34 @@ userSchema.pre('save', function (next) {
                 next();
             });
         });
+    } else {
+        next();
     }
 });
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    // plainPassword 1234567 암호화된 비밀번호
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+userSchema.methods.generateToken = function (cb) {
+    var user = this;
+
+    // jsonwebtoken을 이용해서 token을 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    // user._id + 'secretToken' = token
+    // ->
+    // 'secretToken' -> user._id
+
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return cb(err);
+        cb(null, user);
+    });
+};
 
 const User = mongoose.model('User', userSchema);
 
